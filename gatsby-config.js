215c2be6a98f537c3d0e32c6562d09c6ec247478
Brenden Peters <brenden.peters@gmail.com>
2023-07-22 22:09:42 -0400

    initial setup cont'd

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `bpeters dot app`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.bpeters.app`
   },
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-mdx", {
     resolve: 'gatsby-source-filesystem',
