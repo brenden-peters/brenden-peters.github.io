06222e2afbedbae1e2bc457514cc2023f4ad18a2
Brenden Peters <brenden.peters@gmail.com>
2023-07-30 21:25:45 -0400

    Condense contact blurb to index page, add basic styling, remove more bootstrap example cruft

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import * as React from "react"
-import { Link } from 'gatsby'
+import "../styles/styles.css"
 
-const pageStyles = {
-  color: "#002a15", // #bfff96 base page color, #002a15 base text color
+const pageStyles = { // #bfff96 base page color, #002a15 base text color -- maybe try #FFF4DB as well
+  color: "#002a15", 
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-const headingStyles = {
+const headingStyle = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
 }
-const headingAccentStyles = {
-  color: "#002627",
-}
+
 const paragraphStyles = {
   marginBottom: 48,
 }
+
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
@@ -24,109 +23,26 @@ const codeStyles = {
   fontSize: "1.25rem",
   borderRadius: 4,
 }
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+
+const listStyle = {
+  marginBottom: 16,
+  paddingLeft: -5,
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
+const listItemStyle = {
   fontSize: 16,
   verticalAlign: "5%",
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
+const keywordStyle = {
+	fontSize: 16,
+	fontWeight: "semiBold",
+	color: "#03592e",
 }
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#bfff96", // desired base text color
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
 
 const greetingMessage = ( 
 	<div>
-		<h1 style={headingStyles}>
+		<h1 style={headingStyle}>
 			Hi, I'm Brenden.
 		</h1>
 	</div>
@@ -136,12 +52,18 @@ const greetingMessage = (
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      {greetingMessage}
+      	{greetingMessage}
       <p style={paragraphStyles}>
-	  I build <code style={codeStyles}>software</code>, tinker with hardware, and enjoy growing (and cooking) tasty food. {/*software links to github?*/}
+	  	I build software, tinker with hardware, and enjoy growing (and cooking) tasty food. {/*'software' links to github?*/}
       </p>
 	  <p>
-	  <Link to="/contact">Contact me</Link>
+		Are you...
+		<ul style={listStyle}>
+			<li style={listItemStyle}>Interested in talking about <code style={keywordStyle}>native iOS development</code> or <code style={keywordStyle}>virtual and augmented reality</code>?</li> {/*include link to small briefing about iOS app domains and expertise?*/}
+			<li style={listItemStyle}>Curious about the secrets to making a <code style={keywordStyle}>perfect deli-style sandwich</code>?</li>
+			<li style={listItemStyle}>Looking to connect with talented folks with skills and interests outside my wheelhouse?</li>
+		</ul>
+		Let's chat! You can <a href="mailto:contact@bpeters.app">send me an email</a> or <a href="https://www.linkedin.com/in/brenden-peters/">reach out on LinkedIn</a> {/*expandable contact details section for screen readers? -- what's the most accessible way*/}
 	  </p>
     </main>
   )
